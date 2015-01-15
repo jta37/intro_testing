0e801a13bfa505486108f11a4a271b18c405903f
Josh Andrews <jtandr3ws37@gmail.com>
2015-01-14 19:11:54 -0800

    create migration add PW & PW_confirm to users, creating user in console passing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150115021800) do
+ActiveRecord::Schema.define(version: 20150115031010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,10 @@ ActiveRecord::Schema.define(version: 20150115021800) do
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+    t.string   "password"
+    t.string   "password_confirmation"
   end
 
 end
